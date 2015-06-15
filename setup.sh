f3c0a6b236303ad338e9de0b526ccf6cd9afaf43
jason <jason@8pig.com>
2015-06-15 11:10:09 +0800

    some modification

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
+## forked from git://github.com/ma6174/vim.git, and modified to my own one.
+echo "Installing..."
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
@@ -15,15 +16,15 @@ fi
 sudo easy_install -ZU autopep8 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 mv -f ~/vim ~/vim_old
-cd ~/ && git clone https://github.com/ma6174/vim.git
+cd ~/ && git clone https://github.com/jsongo/vim_jsongo.git
 mv -f ~/.vim ~/.vim_old
-mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
-mv -f ~/.vim/.vimrc ~/
+mv -f ~/vim/.vimrc ~/
+mv -f ~/vim ~/.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "ma6174正在努力为您安装bundle程序" > ma6174
-echo "安装完毕将自动退出" >> ma6174
-echo "请耐心等待" >> ma6174
-vim ma6174 -c "BundleInstall" -c "q" -c "q"
-rm ma6174
+echo "jsongo正在努力为您安装bundle程序" > jsongo
+echo "安装完毕将自动退出" >> jsongo
+echo "请耐心等待" >> jsongo
+vim jsongo -c "BundleInstall" -c "q" -c "q"
+rm jsongo
 echo "安装完成"
